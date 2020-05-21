b18f911cec26be3153f3b30ef8a1c2a86b9de218
Fiona-Mitchell <fionamitchelldesign@gmail.com>
2020-05-21 17:15:13 +0100

    Nav Bar Finished
    Links in mobile form still need to be fixed

--- app.js
+++ app.js
@@ -11,11 +11,12 @@ burger.addEventListener('click', () => {
     if (link.style.animation){
         link.style.animation = ''
     } else{
-      link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 5 + 5}s';
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index/5+0.5}s`;
       console.log(index / 5);
     }
   });
 
+  burger.classList.toggle('toggle');
 
 });
 
